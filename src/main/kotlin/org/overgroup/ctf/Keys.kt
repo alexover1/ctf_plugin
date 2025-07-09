29b908be6e83cfe18d274a3e2ac9b9f0019d4599
Alex Overstreet <47640630+alexover1@users.noreply.github.com>
2025-07-09 01:34:32 -0500

    Remove unused key

--- src/main/kotlin/org/overgroup/ctf/Keys.kt
+++ src/main/kotlin/org/overgroup/ctf/Keys.kt
@@ -4,5 +4,4 @@ import org.bukkit.NamespacedKey
 
 object Keys {
     val CUSTOM_COMMAND = NamespacedKey(App.getInstance(), "CustomCommand")
-    val SHADOWSTEP_MARK_LOCATION = NamespacedKey(App.getInstance(), "ShadowstepMarkLocation")
 }
